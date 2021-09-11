e46b2ebb3507d40a3f88db4ce592922f0aa29de1
nryeouo <43399370+nryeouo@users.noreply.github.com>
2021-09-11 13:36:58 +0900

    add nokana to locale list

--- locales/index.js
+++ locales/index.js
@@ -24,6 +24,7 @@ const languages = [
 	'id-ID',
 	'ja-JP',
 	'ja-KS',
+	'ja-NOKANA',
 	'kab-KAB',
 	'kn-IN',
 	'ko-KR',
@@ -52,6 +53,7 @@ module.exports = Object.entries(locales)
 		switch (k) {
 			case 'ja-JP': return v;
 			case 'ja-KS':
+			case 'ja-NOKANA':
 			case 'en-US': return merge(locales['ja-JP'], v);
 			default: return merge(
 				locales['ja-JP'],
