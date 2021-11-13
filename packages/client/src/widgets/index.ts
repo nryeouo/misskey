a8574033deecb5c620e32c87cffe6262a96e0018
nryeouo <43399370+nryeouo@users.noreply.github.com>
2021-11-13 18:47:23 +0900

    delete sunmoon widget

--- packages/client/src/widgets/index.ts
+++ packages/client/src/widgets/index.ts
@@ -21,7 +21,6 @@ export default function(app: App) {
 	app.component('MkwButton', defineAsyncComponent(() => import('./button.vue')));
 	app.component('MkwAiscript', defineAsyncComponent(() => import('./aiscript.vue')));
 	app.component('MkwAichan', defineAsyncComponent(() => import('./aichan.vue')));
-	app.component('MkwSunmoon', defineAsyncComponent(() => import('./sunmoon.vue')));
 }
 
 export const widgets = [
@@ -45,5 +44,4 @@ export const widgets = [
 	'button',
 	'aiscript',
 	'aichan',
-	'sunmoon',
 ];
